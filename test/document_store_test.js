232a20561559419982d672110dcc13800791c5d8
Matt Wynne <matt@mattwynne.net>
2017-10-30 19:21:03 +0000

    Respond to feedback from attempting to integrate into cucumber pro

--- test/document_store_test.js
+++ test/document_store_test.js
@@ -7,6 +7,15 @@ const MemoryUniqueIndex = require('../lib/index/memory_unique_index')
 describe('DocumentStore', () => {
   const makeId = document => document.uid
 
+  context('being constructed', () => {
+    it('throws an error if given no makeId function', () => {
+      const indices = [
+        new MemoryUniqueIndex({ makeKey: document => document.name, makeId }),
+      ]
+      assert.throws(() => new DocumentStore({ indices }), /makeId is required/)
+    })
+  })
+
   context('with a single unique index', () => {
     it('finds a single document', () => {
       const dave = { name: 'Dave', uid: '1234' }
@@ -42,7 +51,7 @@ describe('DocumentStore', () => {
       assert.equal(store.find({ name: 'Dave' }), null)
     })
 
-    it('throws an error for an invalid query', () => {
+    it('returns null for an invalid query', () => {
       const indices = [
         new MemoryUniqueIndex({ makeKey: document => document.name, makeId }),
       ]
@@ -53,5 +62,52 @@ describe('DocumentStore', () => {
 
   context('with a single grouped index', () => {
     it('finds all documents matching a query')
+  })
+
+  describe('deleting documents', () => {
+    it('deletes an existing document in a single unique index', () => {
+      const dave = { name: 'Dave', uid: '1234' }
+      const store = new DocumentStore({ makeId })
+        .withUniqueIndex(document => document.name)
+        .put(dave)
+        .delete(dave)
+      assert.equal(store.find({ name: 'Dave' }), null)
+    })
+
+    it('deletes existing documents by an indexed query', () => {
+      const dave = { name: 'Dave', uid: '1234' }
+      const store = new DocumentStore({ makeId })
+        .withUniqueIndex(document => document.name)
+        .put(dave)
+        .delete({ name: 'Dave' })
+      assert.equal(store.find({ name: 'Dave' }), null)
+    })
+  })
+
+  context('adding a unique index', () => {
+    it('adds existing documents to the new index', () => {
+      const dave = { name: 'Dave', uid: '1234' }
+      const indices = []
+      const store = new DocumentStore({
+        indices,
+        makeId,
+      })
+        .put(dave)
+        .withUniqueIndex(document => document.name)
+      assert.deepEqual(store.find({ name: 'Dave' }), dave)
+    })
+
+    it('can add multiple indices', () => {
+      const dave = { name: 'Dave', age: 30, uid: '1234' }
+      const indices = []
+      const store = new DocumentStore({
+        indices,
+        makeId,
+      })
+        .put(dave)
+        .withUniqueIndex(document => document.name)
+        .withUniqueIndex(document => document.age.toString())
+      assert.deepEqual(store.find({ age: 30 }), dave)
+    })
   })
 })
